f531793389ff8d414da905c43c18f15c276d6046
Ko Ko Maung <54410761+koko-maung@users.noreply.github.com>
2019-09-19 19:16:23 -0400

    Create myInfo.c

---
+++ myInfo.c
@@ -0,0 +1,13 @@
+/*Instructor-Professor Geiger
+  Ko Ko Maung
+  28/1/2018
+  This program include some basic information about me.*/
+#include <stdio.h>
+int main()
+{
+	printf("Name	-Ko Ko Maung\n");
+	printf("Major	-Computer Engineering\n");
+	printf("Class	-Sophomore second semester\n");
+	printf("EECE.2160: ECE Application Programming\n");
+	return 0;
+}
